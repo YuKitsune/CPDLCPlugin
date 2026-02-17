6baf106ca8f3de547c262518ec0a0a473a217845
Eoin Motherway <25342760+YuKitsune@users.noreply.github.com>
2026-02-17 13:56:08 +1000

    fix: Improve WindowManager error handling

--- source/CPDLCPlugin/Windows/WindowManager.cs
+++ source/CPDLCPlugin/Windows/WindowManager.cs
@@ -14,27 +14,34 @@ public class WindowManager(IGuiInvoker guiInvoker)
     {
         guiInvoker.InvokeOnGUI(mainForm =>
         {
-            lock (_gate)
+            try
             {
-                if (_windows.TryGetValue(key, out var existingWindowHandle))
+                lock (_gate)
                 {
-                    existingWindowHandle.Focus();
-                    return;
+                    if (_windows.TryGetValue(key, out var existingWindowHandle))
+                    {
+                        existingWindowHandle.Focus();
+                        return;
+                    }
+
+                    var windowHandle = new WpfWindowHandle();
+                    var window = createWindow(windowHandle);
+                    windowHandle.SetWindow(window);
+
+                    ElementHost.EnableModelessKeyboardInterop(window);
+
+                    var helper = new System.Windows.Interop.WindowInteropHelper(window);
+                    helper.Owner = mainForm.Handle;
+
+                    window.Closed += (_, _) => TryRemoveWindowInternal(key);
+                    window.Show();
+
+                    _windows[key] = window;
                 }
-
-                var windowHandle = new WpfWindowHandle();
-                var window = createWindow(windowHandle);
-                windowHandle.SetWindow(window);
-
-                ElementHost.EnableModelessKeyboardInterop(window);
-
-                var helper = new System.Windows.Interop.WindowInteropHelper(window);
-                helper.Owner = mainForm.Handle;
-
-                window.Closed += (_, _) => TryRemoveWindowInternal(key);
-                window.Show();
-
-                _windows[key] = window;
+            }
+            catch (Exception ex)
+            {
+                Plugin.AddError(ex, "Failed to open window");
             }
         });
     }
